3802e975bd7b0b6e1dd1f19afb82468223a119c4
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2021-03-21 19:16:07 +0100

    feat: echo statement to debug pipeline

--- plan.sh
+++ plan.sh
@@ -1,3 +1,4 @@
 # Script for What-If deployments
 
+echo 'Start evaluating deployment...'
 az deployment sub what-if --name 'bicep-development' --location 'Switzerland North' -f ./Templates/main.bicep -p location='switzerlandnorth'
